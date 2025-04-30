fb48e1c6347514995f5e56aa99c8f5fa526daa28
Pedro Brito de Souza <144711331+BooneMalay19@users.noreply.github.com>
2025-04-30 01:20:48 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
           <p className="about-subtitle">Os 5 FURIOSOS</p>
           <h2 className="about-title">Aqui é FURIA com <strong>GUARRA</strong></h2>
           <p className="about-text">
-            A FURIA é mais que um time; é uma força que domina o cenário de CS com garra e determinação. Formada por jogadores talentosos e implacáveis, a equipe traz paixão e estratégia para cada partida, inspirando milhões de fãs pelo mundo. Entre para a FURIA e sinta a energia de uma torcida que pulsa a cada bala!
+            A FURIA é mais que um time; é uma força que domina o cenário de CS2 com garra e determinação. Formada por jogadores talentosos e implacáveis, a equipe traz paixão e estratégia para cada partida, inspirando milhões de fãs pelo mundo. Entre para a FURIA e sinta a energia de uma torcida que pulsa a cada bala!
           </p>
           <p className="about-bottom-text">
             <ion-icon name="arrow-forward-circle-outline"></ion-icon>
